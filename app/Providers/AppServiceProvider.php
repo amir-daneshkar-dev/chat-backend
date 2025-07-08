09ebf4c5ab9688d917a5c333c510d7a0680a110a
Amir Daneshkar <amir.daneshkar.90@gmail.com>
2025-07-08 14:44:39 +0330

    set default guard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register broadcasting routes for authentication with CORS support
+        Broadcast::routes(['middleware' => ['api', 'auth:sanctum']]);
     }
 }
